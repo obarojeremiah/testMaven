e5f50ed0771642273a278388552ffca8c762eb4d
Steven De Cock <steven_decock@hotmail.com>
2018-09-24 13:29:49 +0200

    commit branch

---
+++ src/main/java/com/realdolmen/maven/gitmaven/SomeClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.maven.gitmaven;
+
+/**
+ *
+ * @author SDOAX36
+ */
+public class SomeClass {
+    
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    
+}
