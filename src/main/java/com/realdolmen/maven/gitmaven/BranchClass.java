3dd78a5537caeacfa652cec8919355e103b3906c
Steven De Cock <steven_decock@hotmail.com>
2018-09-24 13:08:15 +0200

    commit branch

---
+++ src/main/java/com/realdolmen/maven/gitmaven/BranchClass.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.maven.gitmaven;
+
+/**
+ *
+ * @author SDOAX36
+ */
+public class BranchClass {
+    
+    private String name;
+    private String version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    
+    
+    
+}
