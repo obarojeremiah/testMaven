ea9474cf34b6641774ba4aee5928ec6a7b4805de
obarojeremiah <34315822+obarojeremiah@users.noreply.github.com>
2018-09-24 10:30:21 +0200

    init commit of our maven project

---
+++ src/main/java/com/realdolmen/maven/gitmaven/GitClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.maven.gitmaven;
+
+/**
+ *
+ * @author LJEBG43
+ */
+public class GitClass {
+    
+    public void test(){
+    
+        System.out.println("hello git");
+    }
+    
+}
